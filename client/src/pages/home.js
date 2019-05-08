6921050b7e226f2252356d191b9f9f6a0068a651
Steve Archuleta <stevearchuleta@me.com>
2019-05-07 20:09:29 -0700

    update heroku

---
+++ client/src/pages/home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./home.css"
+
+const Home = () => {
+  return <div className="container">
+  <div className="item logo">LOGO</div>
+  <div className="item header">THE SOUL GARDENERS</div>
+  <div className="item login">LOGIN & PASSWORD</div>
+  <div className="item garden-tips">GARDEN TIPS</div>
+  <div className="item banner">"YOU ARE A SOUL GARDENER!"</div>
+  <div className="item daily-journal">DAILY JOURNAL</div>
+  <div className="item inspiration">INSPRIATION</div>
+  <div className="item main-photo">FLOWER PHOTO</div>
+  <div className="item qa-forum">Q & A FORUM</div>
+  <div className="item tips-div">EMPTY GARDEN TIPS DIV</div>
+  <div className="item footer">FOOTER</div>
+  <div className="item inspiration-div">EMPTY INSPIRATION DIV</div>
+</div>
+}
